efc1b0e20a915017c44478f6efc7529bdc6d9342
Ilya <skyrimfornord337@gmail.com>
2021-02-25 17:17:51 +0500

    Проверка GitHub Pages

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import Panel from "@vkontakte/vkui/dist/components/Panel/Panel";
 import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader";
@@ -10,59 +9,50 @@ import { Cell } from "@vkontakte/vkui/dist/components/Cell/Cell";
 import Div from "@vkontakte/vkui/dist/components/Div/Div";
 import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
 
-const Home = ({ id, go, fetchedUser }) => (
-  <Panel id={id}>
-    <PanelHeader>Example</PanelHeader>
-    {fetchedUser && (
-      <Group
-        header={
-          <Header mode="secondary">User Data Fetched with VK Bridge</Header>
-        }
-      >
-        <Cell
-          before={
-            fetchedUser.photo_200 ? (
-              <Avatar src={fetchedUser.photo_200} />
-            ) : null
-          }
-          description={
-            fetchedUser.city && fetchedUser.city.title
-              ? fetchedUser.city.title
-              : ""
+const Home = ({ id, go, fetchedUser }) => {
+  return (
+    <Panel id={id}>
+      <PanelHeader>Таблица Шульте</PanelHeader>
+      {fetchedUser && (
+        <Group
+          header={
+            <Header mode="secondary">User Data Fetched with VK Bridge</Header>
           }
         >
-          {`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-        </Cell>
+          <Cell
+            before={
+              fetchedUser.photo_200 ? (
+                <Avatar src={fetchedUser.photo_200} />
+              ) : null
+            }
+            description={
+              fetchedUser.city && fetchedUser.city.title
+                ? fetchedUser.city.title
+                : ""
+            }
+          >
+            {`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+          </Cell>
+        </Group>
+      )}
+
+      <Group header={<Header mode="secondary">Navigation Example</Header>}>
+        <Div>
+          <Button
+            stretched
+            size="l"
+            mode="secondary"
+            onClick={() => go("persik")}
+          >
+            Show me the Persik, please
+          </Button>
+        </Div>
       </Group>
-    )}
-
-    <Group header={<Header mode="secondary">Navigation Example</Header>}>
-      <Div>
-        <Button
-          stretched
-          size="l"
-          mode="secondary"
-          onClick={go}
-          data-to="persik"
-        >
-          Show me the Persik, please
-        </Button>
-      </Div>
-    </Group>
-  </Panel>
-);
-
-Home.propTypes = {
-  id: PropTypes.string.isRequired,
-  go: PropTypes.func.isRequired,
-  fetchedUser: PropTypes.shape({
-    photo_200: PropTypes.string,
-    first_name: PropTypes.string,
-    last_name: PropTypes.string,
-    city: PropTypes.shape({
-      title: PropTypes.string,
-    }),
-  }),
+      <Group>
+        <Div>hi</Div>
+      </Group>
+    </Panel>
+  );
 };
 
 export default Home;
